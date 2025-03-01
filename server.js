7001ccfd7f0ca689ffe3105e61ed66b58b704f5b
Aaron Ahn <aaron@countable.ca>
2025-02-28 20:06:43 -0800

    Port update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4325;
 
 app.use(bodyParser.json());
 
