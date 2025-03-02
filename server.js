e5c2b2c914b83f6bc879dc2db19e6750c7b320a5
Aaron Ahn <aaron@countable.ca>
2025-03-01 16:58:13 -0800

    install axios

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const PORT = process.env.PORT || 4325;
